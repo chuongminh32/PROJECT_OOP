aac83031e86a4f4cd34261b3ba4dee33fab9e026
binhminh276 <ntbm27625@gmail.com>
2024-12-14 14:47:11 +0000

    Creat class Staff + Truy cập database + Kiểm thử xuất thông tin

--- src/controllers/StaffController.java
+++ src/controllers/StaffController.java
@@ -1,5 +1,44 @@
 package controllers;
+import java.util.List;
+import java.util.ArrayList;
+import java.sql.Statement; // truy vấn không tham số
+import java.sql.PreparedStatement; // truy vấn có tham số
+import java.sql.Connection; // kết nối db
+import java.sql.SQLException; // lỗi kết nối sql
+import java.sql.ResultSet; // lưu dữ liệu db
+import java.sql.Date;
+import models.Staff; // import class Book
+import utils.DBConnection; // import class DBConnection
 
 public class StaffController {
     
+    public List<Staff> getAllStaffs() throws SQLException {
+        String query = "SELECT * FROM Staff";
+
+        try (Connection connection = DBConnection.getConnection();
+            PreparedStatement statement = connection.prepareStatement(query);
+            ResultSet resultSet = statement.executeQuery()) {
+            List <Staff> staffs = new ArrayList<>();
+
+            while (resultSet.next()) {
+                String id = resultSet.getString("id");
+                String name = resultSet.getString("name");
+                String email = resultSet.getString("email");
+                String phoneNumber = resultSet.getString("phone");
+                String position = resultSet.getString("position");
+                Date hire_date = resultSet.getDate("hireDate");
+    
+                Staff staff = new Staff(id, name, email, phoneNumber, position, hire_date);
+                staffs.add(staff);
+                
+            }
+
+            return staffs;
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+            return new ArrayList<>(); // return an empty list
+        }
+    
+    
+    }
 }
--- src/tests/StaffControllerTest.java
+++ src/tests/StaffControllerTest.java
@@ -1,5 +1,30 @@
 package tests;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.sql.Connection; // kết nối db
+import java.sql.SQLException; // lỗi kết nối sql
+import java.sql.Date;
+import models.Staff; // import class Staff
+import utils.DBConnection; // import class DBConnection
+import controllers.StaffController;
+
 public class StaffControllerTest {
-    
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException{
+        Connection conn = DBConnection.getConnection();
+        StaffController sc = new StaffController();
+        ArrayList<Staff> list = new ArrayList();
+        list = (ArrayList<Staff>) sc.getAllStaffs();
+        int stt = 1;
+        for (int i = 0; i < list.size(); i++){
+            System.out.print(stt +"\t");
+            Staff staff = list.get(i);
+            System.out.print(staff.toString());
+            System.out.println();
+            stt++;
+        }
+        
+    }
+            
 }
